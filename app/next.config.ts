17bbacea4c0923c6eddaeac0776ec77c2a8cec7a
Daniel Jajliardo <danieljajliardo@gmail.com>
2025-09-28 18:35:09 +0000

    2025-09-28T18:34:15Z [web] > nextn@0.1.0 dev 2025-09-28T18:34:15Z [web]

---
+++ app/next.config.ts
@@ -0,0 +1,40 @@
+
+import type {NextConfig} from 'next';
+import withPWA from 'next-pwa';
+
+const isDevelopment = process.env.NODE_ENV === 'development';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+};
+
+const pwaConfig = {
+  dest: 'public',
+  register: !isDevelopment,
+  skipWaiting: !isDevelopment,
+  disable: isDevelopment,
+};
+
+export default withPWA(pwaConfig)(nextConfig);
